e0f52884352899a9bdfd7827cd4ffa1c291bb691
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2014-10-12 21:37:20 -0700

    Initial commit

---
+++ src/main/scala/main.scala
@@ -0,0 +1,15 @@
+package com.biosimilarity.spirograph
+
+import ru.circumflex._, core._, web._, freemarker._
+import java.util.Date
+
+class Main extends Router {
+  val log = new Logger("com.biosimilarity.spirograph")
+
+  'currentDate := new Date
+
+  get("/roadmap") = redirect( "clickableGR.html" )
+  get("/test") = "I'm fine, thanks!"
+  get("/") = ftl("index.ftl")
+
+}
